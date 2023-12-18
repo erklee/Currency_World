284257aeb60f01d2d7ca0f9aabedd556159cfcaf
Eric Lee <erlee92@yahoo.com>
2023-12-18 16:24:44 -0500

    add color click

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import { drawMap } from './scripts/map';
 import { createTooltip, showTooltip, hideTooltip } from './scripts/tooltip';
 import { Country } from './scripts/country';
 import { fetchExchangeRate } from './scripts/currencyapi';
+import currencyHash from "./scripts/currencyhash"
 // import * as chartJS from "chart.js";
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -19,6 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const width = +svg.attr('width');
     const height = +svg.attr('height');
     const tooltip = createTooltip();
+    let selectedCountryPath = null;
   
     const projection = d3.geoMercator().scale(70).center([0, 50]).translate([width / 2, height / 2]);
   
@@ -36,24 +38,30 @@ document.addEventListener('DOMContentLoaded', () => {
         .style('stroke', '#fff');
 
     countryPath
-        .on('mouseover', function (event, d) {
+    .on('mouseover', function (event, d) {
+        if (selectedCountryPath) {
+            selectedCountryPath.attr('fill', 'orange');
+        }
         d3.select(this).attr('fill', 'orange');
         showTooltip(tooltip, event, feature.properties.name);
         })
-        .on('mouseout', function (d) {
+    .on('mouseout', function (d) {
         d3.select(this).attr('fill', '#69b3a2');
         hideTooltip(tooltip);
         })
-        .on('click', async function(event){
+    .on('click', async function(event){
+        let newCountryPath = d3.select(this);
+        // console.log('clicked country:', feature.properties.name);
+        if (selectedCountryPath) {
+            // console.log('previous:', selectedCountryPath.datum().properties.name); 
+            selectedCountryPath.attr('fill', '#69b3a2');
+        }
+        newCountryPath.attr('fill', 'orange');
+        selectedCountryPath = newCountryPath;
+        // console.log('new country', selectedCountryPath.datum().properties.name);
         let country = new Country(feature);
         let oldCurrency = "USD";
-        let currencyHash = {
-            "USA" : "USD",
-            "South Korea" : "KRW",
-            "Canada" : "CAD",
-            "China" : "CNY",
-            "Brazil" : "BRL",
-        };
+    
         let newCurrency = currencyHash[country.name];
         let dates = ["2023-06-01","2023-07-01","2023-08-01","2023-09-01","2023-10-01","2023-11-01"];
         
@@ -65,10 +73,12 @@ document.addEventListener('DOMContentLoaded', () => {
             }));
 
             // console.log("data before chart:", data);
-
+            
             const chartCanvas = document.getElementById('currency_exchange_rate_chart');
             let existingChart = null;
-
+            chartCanvas.width = 400; 
+            chartCanvas.height = 300; 
+            
             // check if chart instance exists
             if (chartCanvas) {
                 const ctx = chartCanvas.getContext('2d');
@@ -83,16 +93,43 @@ document.addEventListener('DOMContentLoaded', () => {
             new Chart(
             document.getElementById("currency_exchange_rate_chart"),
             {
+  
+
                 type: 'line',
                 data: {
-                labels: dates, //labels for your x and y axes
+                labels: dates, //labels for your x
                 datasets: [{
-                    label: "Historical Currency Exchange Rate", //title of graph
+                    label: `Historical Currency Exchange Rate - ${newCurrency}`, //title of graph
                     data: data,
                     fill: false,
                     borderColor: "rgb(75, 192, 192)",
                     tension: 0.1
+                    //maintain aspect ration options
                 }],
+                },
+                options: {
+                    responsive: false, 
+                    maintainAspectRatio: false, 
+                    scales: {
+                        y: {
+                            beginAtZero: false,
+                            title: {
+                                display: true,
+                                text: 'Exchange Rate vs USD' // y axis label
+                            }
+                        }
+                    },
+                    plugins: { 
+                        title: {
+                            display: true,
+                            text: `${country.name}`,
+                            font: {
+                                family: 'Georgia',
+                                size: 18,
+                                weight: 'bold'
+                            }
+                        }
+                    }
                 }
                 
             }
